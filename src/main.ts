386b53fc046678505f603d2b53e87b43c571cd3d
Marx Silva <marxmoita@gmail.com>
2025-04-06 14:59:36 -0300

    chore: change cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'luxboard-api.onrender.com',
+    origin: 'https://lux-board.vercel.app/',
   });
-
   await app.listen(3000);
 }
 bootstrap();
