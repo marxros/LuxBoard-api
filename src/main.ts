dc1c7819a718e9c8a7b1125394fa04ff8884d8d3
Marx Silva <marxmoita@gmail.com>
2025-04-06 14:52:29 -0300

    chore: change cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // ou o endereço do seu frontend
+    origin: 'luxboard-api.onrender.com',
   });
+
   await app.listen(3000);
 }
 bootstrap();
